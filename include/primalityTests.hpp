ecf559c6397ff71bc28861823d3e67751e4c37cc
chafiol <antonin.chafiol@gmail.com>
2020-10-29 20:02:20 +0100

    [ADD] primality Tests files

---
+++ include/primalityTests.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+#include <cmath>
+
+void ptFermat(int p);
+void ptFermat(int a, int p);
+void ptMRab_(int a);
---
+++ src/primalityTests.cpp
@@ -0,0 +1,17 @@
+#include "../include/primalityTests.hpp"
+
+void ptFermat(int p){
+
+}
+
+void ptFermat(const int a, const int p){
+    for (int i = 1; i < p; i++)
+    {
+        if(pow(static_cast<double>(a)))
+    }
+    
+}
+
+void ptMRab_(int a){
+
+}
