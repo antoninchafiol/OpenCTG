c57a8e8752826ceff6b1429e7519b6e04074ee4f
chafiol <antonin.chafiol@gmail.com>
2021-07-12 18:12:41 +0200

    [UPDATE] Need change width from global for and the width used for doing modulo

--- include/basic_crypto.hpp
+++ include/basic_crypto.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 std::string scytale_enc(std::string entry, unsigned int width);
--- src/basic_crypto.cpp
+++ src/basic_crypto.cpp
@@ -5,24 +5,24 @@ using namespace std;
 string scytale_enc(string entry, unsigned int width){
     string res = "";
     cout << entry << endl;
-    char checkcharlist[entry.size()];
+    vector<unsigned int> checkList(entry.size());
+    fill(checkList.begin(),checkList.end(), 0);
 
-    unsigned int lineNumber = (unsigned int)entry.size()/width;
-    unsigned int count = 0;
     for(int j = 0; j < width; j++){   
         for(int i = 0; i<entry.size(); i++){
             if(i%width==0){
-                if(j==width-1){
-                    continue;
-                }
                 cout << entry[i] << endl;
                 res+=entry[i];
+                checkList[i]=1;
             }
-            count++;
-            cout << i << " " << count << " " << count/3 << endl;
         }
         res+=" ";
-        rotate(entry.begin(), entry.begin()+1, entry.end());    
+        for(int i = 0; i<entry.size();i++){
+            if(checkList[i]==1){
+                entry.erase(i);
+            }
+        }
+        width--;
         cout << entry << endl;
     }
     // "shifter" la string de <width>+1 éléments
