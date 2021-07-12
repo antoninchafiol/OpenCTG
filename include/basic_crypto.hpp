1aac368ea5ce7ada72b57591e1d3c7f0c6c5c526
chafiol <antonin.chafiol@gmail.com>
2021-07-12 18:04:57 +0200

    [CHECKPOINT] scytale encrypt near done, checkpoint for another method

--- include/basic_crypto.hpp
+++ include/basic_crypto.hpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 std::string scytale_enc(std::string entry, unsigned int width);
--- src/basic_crypto.cpp
+++ src/basic_crypto.cpp
@@ -3,17 +3,31 @@
 using namespace std;
 
 string scytale_enc(string entry, unsigned int width){
+    string res = "";
+    cout << entry << endl;
     char checkcharlist[entry.size()];
-    for(int i = 0; i<entry.size(); i++){
-        checkcharlist[i]=0;
-    }
-    for(int i =0; i<entry.size(); i++){
-        if(i%2==0){
-            cout << entry[i] << endl;
-            checkcharlist[i]=1;
+
+    unsigned int lineNumber = (unsigned int)entry.size()/width;
+    unsigned int count = 0;
+    for(int j = 0; j < width; j++){   
+        for(int i = 0; i<entry.size(); i++){
+            if(i%width==0){
+                if(j==width-1){
+                    continue;
+                }
+                cout << entry[i] << endl;
+                res+=entry[i];
+            }
+            count++;
+            cout << i << " " << count << " " << count/3 << endl;
         }
+        res+=" ";
+        rotate(entry.begin(), entry.begin()+1, entry.end());    
+        cout << entry << endl;
     }
-    return "";
+    // "shifter" la string de <width>+1 éléments
+    cout << res << endl;    
+    return res;
 }
 string scytale_dec(string entry, unsigned int width){
     return "";
